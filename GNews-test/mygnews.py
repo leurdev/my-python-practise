8f7d7d249be6f91c19b8121485f5d8c3c9636755
Leur Com <64444454+leurdev@users.noreply.github.com>
2022-03-13 14:50:46 +0800

    Update mygnews.py

--- GNews-test/mygnews.py
+++ GNews-test/mygnews.py
@@ -1,9 +1,9 @@
 from gnews import GNews
 
 google_news = GNews()
-json_resp = google_news.get_news('Pakistan')
+json_resp = google_news.get_news('python programming')
 article = google_news.get_full_article(
-    json_resp[0]['url'])  # newspaper3k instance, you can access newspaper3k all attributes in article
+    json_resp[0]['url'])
 
 
 print(article.text)
