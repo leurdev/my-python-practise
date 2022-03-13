cc8171b3c2aee43aafd200c85d3dec3c54b3f227
Leur Com <64444454+leurdev@users.noreply.github.com>
2022-03-13 16:17:10 +0800

    update code

--- WebScrapping_Selenium-test/myselenium.py
+++ WebScrapping_Selenium-test/myselenium.py
@@ -4,12 +4,5 @@ import pandas as pd
 
 driver = webdriver.Chrome()
 
-title = []  # List to store name of the product
-# prices=[] #List to store price of the product
-# ratings=[] #List to store rating of the product
+title = []
 driver.get("https://github.com/leurdev")
-
-content = driver.page_source
-soup = BeautifulSoup(content)
-
-print(soup.get_text())
